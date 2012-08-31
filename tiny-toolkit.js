f169f253d51b72723d3097b80568e26b62025dba
Mike Kamermans <pomax@nihongoresources.com>
2012-08-30 22:29:09 -0400

    nested template conditionals failed. now they don't

--- tiny-toolkit.js
+++ tiny-toolkit.js
@@ -81,7 +81,25 @@
 
   // list of known templates
   var templates = {};
-
+  
+  // recursion is required to actually find all templating blocks
+  var getConditionals = function(string) {
+    var RE = new RegExp("{{#([^}]+)}}(([\\w\\W])*?){{\\/\\1}}",'g'),
+        props = [],
+        match,
+        last,
+        i;
+    // preprocess: conditional blocks
+    while(match = RE.exec(string)) {
+      props.push(match[1]);
+      var found = getConditionals(match[2]);
+      for(i=0, last=found.length; i<last; i++) {
+        props.push(found[i]);    
+      }
+    }
+    return props;
+  };
+  
   /**
    * Insanely simple templating - replace {{moo}} with replacements.moo,
    * then use this template as the content for <element>.
@@ -91,21 +109,20 @@
     if (!templates[templateName]) {
       templates[templateName] = getTPL(templateName+".tpl.html");
     }
-
-    var replaced = templates[templateName], replacement,
-        props = [], i, last=0, prop,
-        RE = new RegExp("{{#([^}]+)}}((\n|.)*?){{\\/\\1}}",'g'), match;
-
-    // preprocess: conditional blocks
-    while(match = RE.exec(replaced)) {
-      props.push(match[1]); last++;
-    }
+    
+    var replaced = templates[templateName],
+        props = [], i, last = 0, prop;
+     
+    props = getConditionals(replaced, props);
+    last = props.length;
 
     for(i=0; i<last; i++) {
       prop = props[i];
-      RE = new RegExp("{{#"+prop+"}}((\n|.)*?){{\\/"+prop+"}}",'g');
+      RE = new RegExp("{{#"+prop+"}}(([\\w\\W])*?){{\\/"+prop+"}}",'g');
       // known property: unwrap for substitution
-      if(exists(replacements[prop])) { replaced = replaced.replace(RE, "$1"); }
+      if(exists(replacements[prop]) && replacements[prop]!==false) {
+        replaced = replaced.replace(RE, "$1");
+      }
       // unknown property: remove entire block
       else { replaced = replaced.replace(RE, ''); }
     }
@@ -113,9 +130,13 @@
     // then perform real substitutions
     for(prop in replacements) {
       if(Object.hasOwnProperty(replacements, prop)) continue;
-      replacement = replacements[prop];
-      replaced = replaced.replace(new RegExp("{{"+prop+"}}",'g'), replacement);
+      // {{prop}} and {{prop | ...} replacement
+      replaced = replaced.replace(new RegExp("{{"+prop+"( | [^}]*)?}}",'g'), replacements[prop]);
     }
+    
+    // do the fallback substitutions for all unmatched {{prop | ...}}
+    replaced = replaced.replace(new RegExp("{{[^|]+\\s*\\|\\s*([^}]*)}}",'g'), "$1");
+
     return replaced;
   }
 
@@ -339,6 +360,7 @@
      * homogenise with set API
      */
     bind(e, "do", function(f) { f(e); return e; });
+    e.length = 1;
 
     // chaining return
     e["__ttk_extended"] = true;
