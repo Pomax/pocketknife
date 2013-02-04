2ccec2b5c3685eb33a2581de40924975485a70f2
Mike Kamermans <pomax@nihongoresources.com>
2013-02-04 17:46:12 -0500

    fixed range for IE

--- extensions/input type range/tiny-toolkit-input-type-range.js
+++ extensions/input type range/tiny-toolkit-input-type-range.js
@@ -18,24 +18,24 @@
   // take input element, hijack set() so that (type,range)
   // triggers a replacement, IF the element is in the DOM.
   (function($) {
-    var original = $.set;
-    $.set = function(name, value) {
+    var originalSetAttribute = $.setAttribute;
+    $.setAttribute = function(name, value) {
       // only intercept type=range
       if (name !== "type" || value !== "range") {
-        return original.apply(this, [name, value]);
+        return originalSetAttribute.apply(this, [name, value]);
       }
       // perform substitution
       if (this.parent()) {
         this.replace(substitute(this));
       }
     }
-  }(HTMLElement.prototype));
+  }(HTMLInputElement.prototype));
 
   /**
    * universal slider repositioning
    */
   function reposition(rails, slider, evt) {
-    if (rails.disabled) return;
+    if (rails.get("disabled") === "disabled") return;
     var x = evt.clientX,
         rpos = rails.position(),
         min = rpos.left,
@@ -45,9 +45,14 @@
       x -= min;
       var ncmin = parseFloat(rails.get("min")),
           ncmax = parseFloat(rails.get("max")),
+          step = parseFloat(rails.get("step")),
           ratio = x / (max - min),
-          value = "" + (ncmin + ratio * (ncmax - ncmin));
-      if (value === oldval) return;
+          value = ncmin + step*(Math.round(ratio * (ncmax - ncmin) / step));
+
+    console.log(ncmin + ", " + ncmax + ", " + step + ", " + ratio + ", " + value);
+
+      if (value > ncmax || value === oldval) return;
+      oldval = value;
       slider.css("left", parseInt(1000 * (value - ncmin) / (ncmax-ncmin)) / 10 + "%");
       slider.set("title",value);
       rails.set("value",value);
@@ -63,20 +68,19 @@
     var props = {
           disabled: false,
           type: "range",
+          id: input.id || "",
           min: input.get("min") || "0",
           max: input.get("max") || "100",
           step: input.get("step") || "1",
-          value: input.get("value") || "0"
+          value: input.get("value") || "0",
+          "class": input.get("class") || "",
+          "style": input.get("style") || ""
         },
         rails = create("div", props);
-
-    if (input.id) rails.id = input.id;
-    if (input.get("class")) rails.set("class", input.get("class"));
-    if (input.get("style")) rails.set("style", input.get("style"));
     if (input.onchange) { rails.onchange = input.onchange; }
     rails.classes().add("tiny-toolkit-input-type-range");
 
-    var slider = create("span").classes().add("tiny-toolkit-input-type-range-slider");
+    var slider = create("span", {"class":"tiny-toolkit-input-type-range-slider"});
     slider.set("title", props.value);
     rails.add(slider);
 
@@ -87,12 +91,6 @@
     rails.onmousedown    = function () { return false; };
 
     // reposition is actually handled by the rails
-    rails.listen("mousemove", function(evt){
-      if (rails.get("sdown")) {
-        reposition(rails, slider, evt);
-      }
-    });
-
     rails.listen("mousedown", function(evt){
       if (evt.which === 1 || evt.button === 1) {
         rails.set("sdown", true);
@@ -101,8 +99,16 @@
       }
     });
 
-    window.document.listen("mouseup", function(evt){
-      rails.set("sdown", "");
+    // but when the mouse is down, response is
+    // handled by the document.
+    document.listen("mousemove", function(evt) {
+      if (rails.get("sdown") === "true") {
+        reposition(rails, slider, evt);
+      }
+    });
+
+    document.listen("mouseup", function(evt) {
+      rails.set("sdown", false);
     });
 
     // make sure the slider starts at the correct position
@@ -116,18 +122,21 @@
 
     // enable/disable is always useful
     rails.disable = function() {
-      rails.disabled = true;
+      rails.set("disabled","disabled");
       rails.classes().add("hidden");
       slider.classes().add("hidden");
     };
     rails.enable = function() {
-      rails.disabled = false;
+      rails.set("disabled","");
       rails.classes().remove("hidden");
       slider.classes().remove("hidden");
     };
 
     return rails;
   }
+
+  // bind to toolkit
+  window["Toolkit"].substitute = substitute;
 
   // general <input type="range"> replacement
   var replaceAllInputRanges = function(ctx) {
